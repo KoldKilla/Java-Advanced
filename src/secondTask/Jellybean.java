2b39d1fd899547d0cffe9c6fa718cecabeef86b9
Konstantin Demidov <88594050+KoldKilla@users.noreply.github.com>
2024-10-21 22:01:09 +0300

    Update Jellybean.java

--- src/secondTask/Jellybean.java
+++ src/secondTask/Jellybean.java
@@ -1,53 +1,23 @@
 package secondTask;
 
-public class Jellybean implements Sweets{
-    String name;
-    int price;
-    int weight;
-    String taste;
+public class Jellybean extends Sweets {
+    private String taste;
 
     public Jellybean(String name, int price, int weight, String taste) {
-        this.name = name;
-        this.price = price;
-        this.weight = weight;
+        super(name, price, weight);
         this.taste = taste;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public int getPrice() {
-        return price;
-    }
-
-    @Override
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    @Override
-    public int getWeight() {
-        return weight;
-    }
-
-    @Override
-    public void setWeight(int weight) {
-        this.weight = weight;
     }
 
     public String getTaste() {
         return taste;
     }
 
-    public void setTaste(String taste) {
-        this.taste = taste;
+    public void setTaste(String flavor) {
+        this.taste = flavor;
+    }
+
+    @Override
+    public String getUniqueParameter() {
+        return "Вкус: " + taste;
     }
 }
