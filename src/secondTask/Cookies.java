d359bc94b72494d6066eff7fcecbaeb8d8e67212
Konstantin Demidov <88594050+KoldKilla@users.noreply.github.com>
2024-10-21 22:00:53 +0300

    Update Cookies.java

--- src/secondTask/Cookies.java
+++ src/secondTask/Cookies.java
@@ -1,46 +1,11 @@
 package secondTask;
 
-public class Cookies implements Sweets{
-    String name;
-    int price;
-    int weight;
-    String calories;
+public class Cookies extends Sweets {
+    private String calories;
 
     public Cookies(String name, int price, int weight, String calories) {
-        this.name = name;
-        this.price = price;
-        this.weight = weight;
+        super(name, price, weight);
         this.calories = calories;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public int getPrice() {
-        return price;
-    }
-
-    @Override
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    @Override
-    public int getWeight() {
-        return weight;
-    }
-
-    @Override
-    public void setWeight(int weight) {
-        this.weight = weight;
     }
 
     public String getCalories() {
@@ -49,5 +14,10 @@ public class Cookies implements Sweets{
 
     public void setCalories(String calories) {
         this.calories = calories;
+    }
+
+    @Override
+    public String getUniqueParameter() {
+        return "Калорийность: " + calories;
     }
 }
