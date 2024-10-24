d5fee23dec873e78fb932f68732352af8f2f175a
Konstantin Demidov <88594050+KoldKilla@users.noreply.github.com>
2024-10-24 19:15:53 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@
  * @author Демидов К. С.
  * @version 1.0
  */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -12,72 +13,89 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
+        final String exceptionText = "Ошибка: некорректный ввод. Попробуйте снова.";
+
         Scanner scanner = new Scanner(System.in);
         Calculate calculator = new Calculate();  // Калькулятор для выполнения операций
         FindMaxString findMaxString = new FindMaxString(); // Объект для поиска самого длинного слова
 
         while (true) {
-            System.out.println("Выберите действие программы: 1 - Калькулятор, 2 - Поиск самого длинного слова");
-            int actionProgram = scanner.nextInt();
-            scanner.nextLine(); // Очистка буфера после ввода числа
-
-            if (actionProgram == 1) {
-                try {
-                    // Ввод чисел для калькуляции
-                    System.out.println("Введите первое число: ");
-                    double firstNumber = scanner.nextDouble();
-                    calculator.setFirstNumber(firstNumber);
-
-                    System.out.println("Введите второе число: ");
-                    double secondNumber = scanner.nextDouble();
-                    calculator.setSecondNumber(secondNumber);
-
-                    System.out.println("Введите действие: 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление");
-                    int mathsAction = scanner.nextInt();
-
-                    // Получение и вывод результата
-                    double result = calculator.calculate(mathsAction);
-                    System.out.printf("Результат: %.4f%n", result);
-
-                } catch (IllegalArgumentException | ArithmeticException e) {
-                    System.out.println("Ошибка: " + e.getMessage());
-                }
-
-                // Продолжить или завершить
-                System.out.println("Вы хотите продолжить? (Y/N)");
-                String inputChoice = scanner.next();
-                if (inputChoice.equalsIgnoreCase("N")) break;
-
-            } else if (actionProgram == 2) {
-                // Ввод размера массива
-                System.out.print("Введите количество слов: ");
-                int size = scanner.nextInt();
+            try {
+                System.out.println("Выберите действие программы: 1 - Калькулятор, 2 - Поиск самого длинного слова");
+                int actionProgram = scanner.nextInt();
                 scanner.nextLine(); // Очистка буфера после ввода числа
 
-                // Создаем массив слов
-                String[] words = new String[size];
+                if (actionProgram == 1) {
+                    try {
+                        // Ввод чисел для калькуляции
+                        System.out.println("Введите первое число: ");
+                        double firstNumber = scanner.nextDouble();
+                        calculator.setFirstNumber(firstNumber);
 
-                // Ввод слов
-                System.out.println("Введите слова:");
-                for (int i = 0; i < size; i++) {
-                    System.out.print("Слово " + (i + 1) + ": ");
-                    words[i] = scanner.nextLine();
+                        System.out.println("Введите второе число: ");
+                        double secondNumber = scanner.nextDouble();
+                        calculator.setSecondNumber(secondNumber);
+
+                        System.out.println("Введите действие: 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление");
+                        int mathsAction = scanner.nextInt();
+
+                        // Получение и вывод результата
+                        double result = calculator.calculate(mathsAction);
+                        System.out.printf("Результат: %.4f%n", result);
+
+                    } catch (IllegalArgumentException | ArithmeticException e) {
+                        System.out.println(exceptionText);
+                    } catch (InputMismatchException e) {
+                        System.out.println(exceptionText);
+                        scanner.nextLine();  // Очистка буфера ввода
+                    }
+
+                    // Продолжить или завершить
+                    if (!AskToContinue.askToContinue(scanner)) {
+                        break;
+                    }
+
+                } else if (actionProgram == 2) {
+                    try {
+                        // Ввод размера массива
+                        System.out.print("Введите количество слов: ");
+                        int size = scanner.nextInt();
+                        scanner.nextLine(); // Очистка буфера после ввода числа
+
+                        // Создаем массив слов
+                        String[] words = new String[size];
+
+                        // Ввод слов
+                        System.out.println("Введите слова:");
+                        for (int i = 0; i < size; i++) {
+                            System.out.print("Слово " + (i + 1) + ": ");
+                            words[i] = scanner.nextLine();
+                        }
+
+                        // Поиск самого длинного слова
+                        String longestWord = findMaxString.findLongestWord(words);
+                        System.out.println("Самое длинное слово: " + longestWord);
+
+                        // Продолжить или завершить
+
+                        if (!AskToContinue.askToContinue(scanner)) {
+                            break;
+                        }
+                    } catch (NegativeArraySizeException e) {
+                        System.out.println(exceptionText);
+                    } catch (InputMismatchException e) {
+                        System.out.println(exceptionText);
+                        scanner.nextLine();  // Очистка буфера ввода
+                    }
+
+                } else {
+                    System.out.println("Ошибка: Некорректный выбор программы.");
                 }
-
-                // Поиск самого длинного слова
-                String longestWord = findMaxString.findLongestWord(words);
-                System.out.println("Самое длинное слово: " + longestWord);
-
-                // Продолжить или завершить
-                System.out.println("Вы хотите продолжить? (Y/N)");
-                String inputChoice = scanner.next();
-                if (inputChoice.equalsIgnoreCase("N")) {
-                    System.out.println("Пока!");
-                    break;
-                }
-            } else {
-                System.out.println("Ошибка: Некорректный выбор программы.");
+            } catch (InputMismatchException e) {
+                System.out.println(exceptionText);
+                scanner.nextLine();  // Очистка буфера ввода
             }
+
         }
 
         scanner.close();
